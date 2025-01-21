73325dbcc07b119b2a1f6b8f7095661d4abf1551
Alexis Meniante <alexismeniante@MacBook-Air-de-Alexis.local>
2025-01-21 16:44:36 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/mycitymove-explorer3/' : '/',
+  base: '/mycitymove-explorer3/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
