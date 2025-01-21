e3fb0fb2951f5f931218d8b40a9a86fb4a3191ae
Alexis Meniante <alexismeniante@MacBook-Air-de-Alexis.local>
2025-01-21 17:01:50 +0100

    Fix: Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mycitymove-explorer3/',
+  base: '/mycitymove-explorer-web/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
