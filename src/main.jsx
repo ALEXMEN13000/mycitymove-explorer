f5bd7eb0913aefb14eebc05751ced9d5386e3c99
Alexis Meniante <alexismeniante@MacBook-Air-de-Alexis.local>
2025-01-21 18:45:52 +0100

    Update configuration for new repository

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/clubcenter">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-) 
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mycitymove-explorer-web/',
+  base: '/mycitymove-explorer/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
